e3b3666b747b19f681d0a4287ff81bd6ce015b22
base0 <vacapp@live.com>
2015-12-23 12:00:04 +0700

    Update frm_pdf.js

--- frm_pdf.js
+++ frm_pdf.js
@@ -1,18 +1,21 @@
-function myFunction(e) {
-  var emailCol = 3
-  var file = DriveApp.getFileById(DocumentApp.getActiveDocument().getId()).makeCopy('applications/application' + Math.random());
+function onFormSubmit(e) {
+  var docId = '1cup0TbrQ7CamZCFRFj5bp7-H1v2kGiZjOf901lZB8eU';
+  var email = e.response.getItemResponses()[1].getResponse();
+  
+  var file = DriveApp.getFileById(docId).makeCopy('applications/application' + Math.random());
   var newDoc = DocumentApp.openById(file.getId());
   
-  replace(newDoc);
-
+  replace(newDoc, e);
+  newDoc.saveAndClose();
+  
   var pdf = newDoc.getAs('application/pdf').getBytes();
   var attach = {fileName:'application form.pdf', content:pdf, mimeType:'application/pdf'};
-  MailApp.sendEmail('wannik.academy@gmail.com', 'asdf', 'bbb', {attachments:[attach]});
+  MailApp.sendEmail(email, 'แบบฟอร์มรับสมัคร', 'กรุณาพิมพ์ไฟล์แนบและติดรูปถ่ายเพื่อยื่นในวันสมัคร', {attachments:[attach]});
 }
 
-function replace(newDoc) {
+function replace(newDoc, e) {
   var body = newDoc.getBody();
 
-  body.replaceText('{name}', 'วรเศรษฐ');
-  body.replaceText('{email}', 'a');
+  body.replaceText('<name>', e.response.getItemResponses()[0].getResponse());
+  body.replaceText('<email>', e.response.getItemResponses()[1].getResponse());
 }
