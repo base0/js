bfe800a31d409e1f6c7c326b3c3f3a8a3544edc5
base0 <vacapp@live.com>
2015-12-23 12:19:58 +0700

    Update frm_pdf.js

--- frm_pdf.js
+++ frm_pdf.js
@@ -1,8 +1,8 @@
 function onFormSubmit(e) {
-  var docId = '1cup0TbrQ7CamZCFRFj5bp7-H1v2kGiZjOf901lZB8eU';
-  var email = e.response.getItemResponses()[1].getResponse();
+  var docId = 'xxxx';
+  var email = e.response.getItemResponses()[xxxx].getResponse();
   
-  var file = DriveApp.getFileById(docId).makeCopy('applications/application' + Math.random());
+  var file = DriveApp.getFileById(docId).makeCopy('application' + Math.random());
   var newDoc = DocumentApp.openById(file.getId());
   
   replace(newDoc, e);
@@ -11,11 +11,13 @@ function onFormSubmit(e) {
   var pdf = newDoc.getAs('application/pdf').getBytes();
   var attach = {fileName:'application form.pdf', content:pdf, mimeType:'application/pdf'};
   MailApp.sendEmail(email, 'แบบฟอร์มรับสมัคร', 'กรุณาพิมพ์ไฟล์แนบและติดรูปถ่ายเพื่อยื่นในวันสมัคร', {attachments:[attach]});
+  
+  DriveApp.removeFile(file);
 }
 
 function replace(newDoc, e) {
   var body = newDoc.getBody();
 
-  body.replaceText('<name>', e.response.getItemResponses()[0].getResponse());
-  body.replaceText('<email>', e.response.getItemResponses()[1].getResponse());
+  body.replaceText('<name>', e.response.getItemResponses()[xxxx].getResponse());
+  body.replaceText('<email>', e.response.getItemResponses()[xxx].getResponse());
 }
