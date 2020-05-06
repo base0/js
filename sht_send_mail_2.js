8909b837aaa8e23078b1338b03a084b9a86825ee
base0 <vacapp@live.com>
2020-05-06 12:45:30 +0700

    Update sht_send_mail_2.js

--- sht_send_mail_2.js
+++ sht_send_mail_2.js
@@ -1,21 +1,27 @@
+const SHEET_NAME = "sheet 1";
+const TEST = true;             // if true, only first row is used
+
 function myFunction() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheetByName("unclaim");
+  var sheet = ss.getSheetByName(SHEET_NAME);
   var range = sheet.getDataRange();
   var data = range.getValues();
-  for (var i = 0; i < data.length; i++) {
+  var N = TEST ? 1 : data.length;
+  for (var i = 0; i < N; i++) {
     var row = data[i];
     Logger.log(row[0]+row[1]+row[2]);
+    sendMail(row[0], row[1], row[2]);
   }
 }
 
 function sendMail(recipient, name, url) {
-  var body = `สวัสดีครับ
+  var subject = "";
+  var body = `สวัสดีครับคุณ NAME
 
 ขอแสดงความนับถือ
 
-base zero`;
+`;
 
-   MailApp.sendEmail(recipient, "subject", body);
+   body = body.replace("NAME", name);
+   MailApp.sendEmail(recipient, subject, body);
 }
-
